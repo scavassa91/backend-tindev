3901b5a107d03f0ac045a6facf230d52931de496
Danilo Bevilacqua Scavassa <danilo.scavassa@venturus.org.br>
2019-08-14 15:02:14 -0300

    tindev backend initial commit

---
+++ src/controllers/LikeController.js
@@ -0,0 +1,40 @@
+const Dev = require('../models/Dev');
+
+module.exports = {
+    async store(req, res) {
+        try {
+            const { user } = req.headers;
+            const { devId } = req.params;
+
+            const loggedDev = await Dev.findById(user);
+            const targetDev = await Dev.findById(devId);
+
+            if (!targetDev) {
+                return res.status(400).json({ error: 'Dev not existis' });
+            }
+
+            if (targetDev.likes.includes(loggedDev._id)) {
+                const loggedSocket = req.connectedUsers[user];
+                const targetSocket = req.connectedUsers[devId];
+
+                if (loggedSocket) {
+                    req.io.to(loggedSocket).emit('match', targetDev);
+                }
+
+                if (targetSocket) {
+                    req.io.to(targetSocket).emit('match', loggedDev);
+                }
+            }
+
+            loggedDev.likes.push(targetDev._id);
+
+            console.log(loggedDev);
+
+            await loggedDev.save();
+
+            return res.json(loggedDev);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+};
